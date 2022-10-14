864ac18b04a1419c26b1a78710b583cf2cef271e
tfk1815 <ayinlataofeek8624@gmail.com>
2022-10-14 11:01:52 -0700

    task o

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,17 @@ int main(void)
 	if (n == 0)
 
 	{
-		printf("The number you entered is zero\n");
+		printf("is zero\n");
 	}
 
 	else if (n > 0)
 	{
-		printf("The number you entered is a positive number\n");
+		printf("is positive\n");
 	}
 
 	else if (n < 0)
 	{
-		printf("The number you entered is a negative number\n");
+		printf("is negative\n");
 	}
 
 	return (0);
