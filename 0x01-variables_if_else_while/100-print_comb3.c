08417327d77b13ff2edd30c3d77266686a7d873e
tfk1815 <ayinlataofeek8624@gmail.com>
2022-10-14 16:23:08 -0700

    advanced task 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main-Entry
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num1,num2 = '0',j;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for(j = num2; j <= '9'; j++)
+		{
+			if (num1 != j)
+			{
+				putchar(num1);
+				putchar(j);
+			}
+			if (num1 == j)
+			{
+				continue;
+			}
+			if (num1 == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(' ');
+				putchar(' ');
+			}
+		}
+		num2++;
+	}
+	putchar('\n');
+	return (0);
+}
