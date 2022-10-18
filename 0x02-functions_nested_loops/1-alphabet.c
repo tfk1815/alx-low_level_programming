b029fd9bad823881a024a8ad4ab7ff014083d839
tfk1815 <ayinlataofeek8624@gmail.com>
2022-10-18 14:49:28 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * print_alphabet- prints all alphabets in lowercase.
+ * Return: on success 1.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
